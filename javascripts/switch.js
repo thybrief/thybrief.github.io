85aa33b50512ec22da0c9fd91f9126e60283e651
thybrief <nirbhay@thybrief.co>
2016-03-23 14:14:07 +0530

    final changes

---
+++ javascripts/switch.js
@@ -0,0 +1,92 @@
+
+function change() {
+
+  var x = document.getElementById('partner');
+
+  x.style.width="100%";
+  x.style.height="100%";
+  x.style.top="30px";
+  x.style.position="fixed";
+  x.style.padding="15% 0 0 0";
+  x.style.margin="20px 0 0 0";
+  x.style.display="block";
+  x.style.left="0px";
+  x.style.fontSize="40px";
+  x.style.index="100";
+  x.style.background="rgba(255,355,255,0.9)";
+  x.onclick=function(){change1()};
+
+  var y=document.getElementById("foot");
+  y.style.display="none";
+
+
+};
+
+function change1() {
+var x = document.getElementById('partner');
+
+      x.style.height="auto"
+      x.style.position="relative";
+      x.style.padding="0px 0px 0px 0px";
+      x.style.top="0px";
+      x.style.textAlign="center"
+      x.style.display="block";
+      x.style.left="none";
+      x.style.fontSize="16px";
+      x.style.index="auto";
+      x.style.background="rgba(255,355,255,1)";
+      x.onclick=function(){change()};
+      var y=document.getElementById("foot");
+      y.style.display="block";
+};
+
+function mustread(){
+  var x=document.getElementById('mustread');
+  x.style.display="block";
+  x.style.position="fixed";
+  x.style.width="100%";
+  x.style.height="100%";
+  x.style.zIndex="100";
+  x.style.background="rgba(255,255,255,0.7)";
+  x.onclick=function(){mustunread()};
+
+}
+function mustunread(){
+  var x=document.getElementById('mustread');
+  x.style.display="none";
+
+  x.style.position="fixed";
+  x.style.width="100%";
+  x.style.height="auto";
+  x.style.zIndex="100000";
+  x.style.background="rgba(255,255,255,0.7)";
+  x.onclick=function(){unabout()};
+
+}
+
+function about(){
+  var x=document.getElementById('about');
+  x.style.display="block";
+  x.style.position="fixed";
+  x.style.width="100%";
+  x.style.height="100%";
+  x.style.zIndex="100";
+  x.style.top="0px";
+  x.style.padding="18% 0 0 0";
+  x.style.left="0px";
+  x.style.background="rgba(255,255,255,0.9)";
+  x.onclick=function(){unabout()};
+
+}
+
+function unabout(){
+  var x=document.getElementById('about');
+  x.style.display="none";
+  x.style.position="relative";
+  x.style.width="100%";
+  x.style.height="auto";
+  x.style.zIndex="100000";
+  x.style.background="rgba(255,255,255,0.7)";
+  x.onclick=function(){about()};
+
+}
